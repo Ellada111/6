5f6b78a5853a517810550be2d0bf3984281d9f7c
Ellada111 <141130196+Ellada111@users.noreply.github.com>
2023-08-30 07:59:40 +0600

    Add files via upload

---
+++ DOM/index.js
@@ -0,0 +1,27 @@
+// document.body.style.background = "pink"
+
+// document.getElementsByTagName ("h1") 
+
+// const titleElement = document.getElementById ('title')
+
+// titleElement.style.color = 'red'
+
+// // const paraph = document. querySelector ('.paraph');
+// // const paraph = document.querySelector ('#paraphId') // ID
+// // const paraph = document.querySelector ('p') ; 
+// // console log (paraph)
+
+// const paraphs = document.getElementsByClassName ('paraph')
+// console.log(paraphs);
+
+
+// const listElement = document.querySelector ('#list');
+
+// listElement.innerHTML = 
+ 
+
+//  const vegetablelist =  document.getElementById('vegetables')
+//  console.log (vegetablelist.outerHTML);
+//  vegetablelist.outerHTML=
+ 
+
